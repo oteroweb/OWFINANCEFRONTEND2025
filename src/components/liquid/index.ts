d0b72edf3adbdd903e66ddb5744719a2b345100d
oteroweb <oterolopez1990@gmail.com>
2026-04-04 12:35:59 -0400

    feat(lite-ui): componentes Stitch funcionales, menú con iconos, soporte dark/light

---
+++ src/components/liquid/index.ts
@@ -0,0 +1,5 @@
+export { default as LiteHeaderMobile } from './LiteHeaderMobile.vue';
+export { default as LiteHeaderDesktop } from './LiteHeaderDesktop.vue';
+export { default as LiteBottomNavMobile } from './LiteBottomNavMobile.vue';
+export { default as LiteBottomNavDesktop } from './LiteBottomNavDesktop.vue';
+export { default as ExpandedNavigationMenuLight } from './ExpandedNavigationMenuLight.vue';
